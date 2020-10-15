7662bb886badfb235f968d48a99b8db8348218cd
shinnosuke7 <shin2000902@icloud.com>
2020-10-15 16:42:21 +0900

    main.py 合計ファイルから、計算された値を読み込んで、その値を保存用エクセルファイルの日にちの欄に入れ込む。

--- syuya.py
+++ syuya.py
@@ -5,7 +5,7 @@ dirname= os.getcwd() #絶対参照というか自分の今いる場所がわか�
 #print(dirname) /uriageになる
 save_folder_path =os.path.join(dirname,'保存先')#ここで売り上げの中の保存先っているディレクトリに行ってねって指示できている
 w_path= os.path.join(dirname,'書き込み用エクセルファイル.xlsm')#os.path.joinっているのはパスをつなげるときに必要、joinだけだとできない
-natural_save_excel = os.path.join(save_folder_path,'shuya_natural保存先.xlsx')
+natural_save_excel = os.path.join(save_folder_path,'shuya_natural保存先.xlsx')#しゅうやの保存用原本にアクセスしている
 w_Book = xw.Book(w_path)#書き込み用エクセルファイルのパスのファイルを読み込んで、それをw_bookと置いて活用しやすいようにする
 w_year =str(w_Book.sheets['売り上げ記入用'].range('F2').value)#書き込み用エクセルファイルで選択された年の値を読み取る
 w_Book.close()#いったん閉じる　無駄なリソースを使わないために
@@ -17,7 +17,8 @@ wb_2020.close()
 t_2020 = os.path.getctime(path_dict['2020.xlsx'])#getctimeとは　そのファイルが最後に更新された時の日時を取得するメソッド
 d_2020 = datetime.datetime.fromtimestamp(t_2020)#fromtimestampはスタンプ値を日本の表記にしている
 year_2020 = d_2020.year #先ほど直した表記から年の値を読み取る
-save_file_path_2020 = os.path.join(save_folder_path,'{}年保存先'.format(year_2020))#保存したいエクセルファイルのあるディレクトリを指定している
+
+save_file_path_2020 = os.path.join(save_folder_path,'{}年保存先'.format(year_2020))#保存したいエクセルファイルのあるディレクトリを指定してそのなかにあるファイルを参照するための年数を指定している　ここでは迄エクセルファイルにアクセルしていない
 save_file_path_2020_excel = os.path.join(save_file_path_2020,'.xlsx')#先ほどのディレクトリから読み込みたいエクセルファイルを指定する
 path_dict = {'{}.xlsx'.format(w_year):os.path.join(save_folder_path,'{}年保存先.xlsx'.format(w_year))}#書き込み用エクセルファイルかかれた
 #年と対応する年の保存用エクセルファイルを読み込む
@@ -36,6 +37,7 @@ s_path= os.path.join(year_dict['{}年.xlsx'.format(w_year)])#保存先エクセ�
 xw1 = xw.Book(w_path)#書き込み用エクセルファイルを変数に置く
 xw2 = xw.Book(s_path)#保存先エクセルファイルを変数に置く
 sheet1 = xw1.sheets['売り上げ記入用']#書き込み用エクセルファイルの売り上げ記入用シートをsheet1遠く
+
 try:#例外処理の対応をするために使う
     def day_sum():
         def osi_sum():#おしぼりの合計の値を調べる　なんで値をforで回していないかっていうと、エクセルの使用でできないらしい。　だから仮の文字を入れておいて、それを文字で回す
