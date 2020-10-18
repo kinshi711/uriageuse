5b5ff7e40df7d9fdfb4a58aee76830a77e4bb2ea
shuya1117 <shuya.wins.forever@gmail.com>
2020-10-19 00:24:40 +0900

    fjaljflaj

--- main.py
+++ main.py
@@ -1,95 +1,73 @@
-import os
 import xlwings as xw
+import os
+import numpy as np
+
+path_dict = {}  # 空辞書を作って　パスを追加していく
+dir = os.getcwd()  # 絶対参照というか自分の今いる場所がわかる /uriage
+path_dict['w_path'] = os.path.join(dir, '書き込み用エクセルファイル.xlsm')  # os.path.joinっているのはパスをつなげるときに必要、joinだけだとできない
+w_book= xw.Book(path_dict['w_path'])  # 書き込み用のエクセルファイルを開く
 
 
-dirname= os.getcwd() #絶対参照というか自分の今いる場所がわかる /uriage
-w_path= os.path.join(dirname,'書き込み用エクセルファイル.xlsm')#os.path.joinっているのはパスをつなげるときに必要、joinだけだとできない
+month = int(w_book.sheets['売り上げ記入用'].range('G1').options(empty=0).value)  # 書き込まれた月の参照
+day = int(w_book.sheets["売り上げ記入用"].range('I1').options(empty=0).value)  # 書き込まれた日の参照
+s_dir = os.path.join(dir, '保存先')  # 保存用フォルダーの参照
+year = int(w_book.sheets['売り上げ記入用'].range('F2').options(empty=0).value)  # 書き込まれた年度を参照
+path_dict['s_path'] = os.path.join(s_dir, '{}年保存先.xlsx'.format(year))  # 保存用エクエルファイルのパス
+path_dict['original_path'] = os.path.join(s_dir, 'shuya_natural保存先.xlsx')  # 原本のエクセルファイルのパス
+path_dict['2020.xlsx'] = os.path.join(s_dir, '2020年保存先.xlsx')  # 2020年のエクセルファイル作成時のpathを呼び出すのを追加
+wb_2020 = xw.Book()
+original_book = xw.Book(path_dict['original_path'])
 
-w_Book = xw.Book(w_path)#書き込み用エクセルファイルのパスのファイルを読み込んで、それをw_bookと置いて活用しやすいようにする
+if path_dict['2020.xlsx'] in s_dir:
+    copy_value=np.array(original_book.sheets[num].range('A1:F32').options(empty=0).value for num in (0,14,1))
+    wb_2020.close()
+else:
+    wb_2020.save(path_dict['2020.xlsx'])
+    copy_value= np.array(original_book.sheets[num].range('A1:F32').options(empty=0).value for num in (0,14,1))
+    sheet_tapple = ('年','12月','11月','10月','9月','8月','7月','6月','5月','4月','3月','2月','1月')
+    for sheet_tapple in ('年','12月','11月','10月','9月','8月','7月','6月','5月','4月','3月','2月','1月'):
+        wb_2020.sheets.add(name='{}'.format(sheet_tapple))
+    wb_2020 = copy_value
+s_book= xw.Book() # 新規保存用のエクセルファイル
+if   path_dict['s_path'] in s_dir:  # 新規保存用のエクセルファイルがフォルダーに既に有ったら　
+     s_book.close()# 新規でエクセルファイルもいらないので消す
+elif path_dict['w_path'] in path_dict['s_path']:
+    s_book.close()
+else:
+    s_book.save(path_dict['{}.xlsx'.format(year)])
+    s_book.close()#保存用のファイルがフォルダーに追加されたので消す。
+w_sheet= w_book.sheets['売り上げ記入用']# 売り上げ記入用
 
-#書き込み用エクセルファイルは開けている
-w_year =str(w_Book.sheets['売り上げ記入用'].range('F2').value)#書き込み用エクセルファイルで選択された年の値を読み取る
-w_month = str(w_Book.sheets['売り上げ記入用'].range('G1').value)
-w_day = str(w_Book.sheets["売り上げ記入用"].range('I1').value)
 
-save_folder_path = os.path.join(dirname,'保存先')
-s_path = os.path.join(save_folder_path,'{}年保存先.xlsx'.format(w_year))
-s_Book = xw.Book(s_path)
-
-xw1 = xw.Book(w_path)
-
-w_year =int(xw1.sheets['売り上げ記入用'].range('F2').options(empty=2020).value)
-s_path= os.path.join(save_folder_path,'{}年保存先.xlsx'.format(w_year))
-xw2 = xw.Book(s_path)
-sheet1 = xw1.sheets['売り上げ記入用']
-
-def osi_sum():
-    o_sum = sum([sheet1.range('B{}'.format(n1)).options(empty=0).value * sheet1.range('C{}'.format(n1)).options(empty=0).value for n1 in range(1, 5, 1)])
+def osibori_sum():
+    o_sum = sum(
+        [w_sheet.range('B{}'.format(n1)).options(empty=0).value * w_sheet.range('C{}'.format(n1)).options(empty=0).value
+         for n1 in range(1, 5, 1)])
 def water_sum():
-    w_sum = sum([sheet1.range('B{}'.format(n2)).options(empty=0).value * sheet1.range('C{}'.format(n2)).options(empty=0).value for n2 in range(5, 10, 1)])
+    w_sum = sum(
+        [w_sheet.range('B{}'.format(n2)).options(empty=0).value * w_sheet.range('C{}'.format(n2)).options(empty=0).value
+         for n2 in range(5, 10, 1)])
+
 def ice_sum():
-    i_sum = sum([sheet1.range('B{}'.format(n3)).options(empty=0).value * sheet1.range('C{}'.format(n3)).options(empty=0).value for n3 in range(10, 17, 1)])
+    i_sum = sum(
+        [w_sheet.range('B{}'.format(n3)).options(empty=0).value * w_sheet.range('C{}'.format(n3)).options(empty=0).value
+         for n3 in range(10, 17, 1)])
+
 def day_sum():
-    osibori = osi_sum()
+    osibori = osibori_sum()
     water = water_sum()
     ice = ice_sum()
     d_sum = osibori + water + ice
 
 
 def month_sum():
-    for m in range(1,13,1):
-        sheet2 = xw2.sheets.range('{}月'.format(m)).options(empty=0).value
-        m_sum= sum([sheet2['E{}'.format(num)].value for num in (2,32,1)])
+    for m in range(1, 13, 1):
+        m_sheet = s_book.sheets.range('{}月'.format(m)).value
+        m_sum = sum([m_sheet.range('E{}'.format(num)).options(empty=0).value for num in (2, 32, 1)])
         return m_sum
+
+
 def year_sum():
-    y_sheet = xw2.sheets['年'].range('B2:B13').options(empty=0).value[0:][0:]
+    y_sheet = s_book.sheets['年'].range('B2:B13').options(empty=0).value[0:][0:]
     y_sum = sum(y_sheet)
     return y_sum
-
-
-xw1.close()
-xw2.close()
-
-natural_save_excel = os.path.join(save_folder_path, 'shuya_natural保存先.xlsx')
-path_dict = {}  # 空辞書を作って　パスを作っておく
-
-w_Book = xw.Book(w_path)  # 書き込みエクセルファイルの参照
-w_year = int(w_Book.sheets['売り上げ記入用'].range('F2').options(empty=2020).value)  # 書き込みエクセルファイルに書かれた年度を参照
-path_dict['2020.xlsx'] = os.path.join(save_folder_path, '2020年保存先.xlsx')
-# path_dictは　2020年のエクセルファイル作成時のpathを呼び出す。
-
-if path_dict['2020.xlsx'] in save_folder_path:
-    wb2020 = xw.Book(path_dict['2020.xlsx'])
-    wb2020.close()
-else:
-    wb2020 = xw.Book()  # 2020年保存先のエクセルファイルの作成
-    wb2020.save(path_dict['2020.xlsx'])  # 保存先の指
-    for m in range(1, 13, 1):
-        natural_month_sheet = xw.Book(natural_save_excel).sheets['{}月'.format(m)]
-        natural_year_sheet = xw.Book(natural_save_excel).sheets('年')
-        copy_month_value = natural_month_sheet.range('A1:F32').options(empty=0).value
-        copy_year_value = natural_year_sheet.range('A1:C13').options(empty=0).value
-        wb2020.sheets.add(name='{}月'.format(m))
-        wb2020.save()
-
-path_dict['{}.xlsx'.format(w_year)] = os.path.join(save_folder_path, '{}年保存先.xlsx'.format(w_year))
-year_Book = xw.Book()  # エクセルを作成
-if path_dict['{}.xlsx'.format(w_year)] in save_folder_path:
-    year_Book.close()
-else:
-    # 書き込みエクセルファイルの年を取得してそれをもとにエクセルファイル名を取得してその上でそこのパスを作成している
-    year_Book.save(path_dict['{}.xlsx'.format(w_year)])  # pathを指定して保存する
-    year_Book.close()  # 書かれた年の作成ファイルを閉じる
-
-
-w_Book.close()  # 書き込みファイル閉じる
-xw.Book(natural_save_excel).close()  # 原本ファイル閉じる
-
-
-def main_to_save():
-    a = osi_sum()
-    print(a)
-
-main_to_save()
-#    s_Book.sheets['{}月'.format(w_month)].range('B{}'.format(w_month))
-
